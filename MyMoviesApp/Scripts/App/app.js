86302e20de906c552f33291b6e7d820df5e758cc
waafre13 <waafre13@student.westerdals.no>
2015-12-17 17:22:20 +0100

    Updated TODOs for views

--- MyMoviesApp/Scripts/App/app.js
+++ MyMoviesApp/Scripts/App/app.js
@@ -23,4 +23,34 @@
 
     }]);
 
+    myMoviesApp.controller("MoviesController", ["$scope", function ($scope) {
+
+        $scope.test = "Fredric";
+
+
+    }]);
+
+    myMoviesApp.controller("ReviewsController", ["$scope", function ($scope) {
+
+        $scope.test = "Fredric";
+
+        
+
+    }]);
+
+    myMoviesApp.factory("MoviesFactory", [
+        function() {
+
+            //TODO: FACTORY: getMovies, getMovie(id)r object, updateMovie()
+
+            return {
+                
+                getMovies: function() {
+                    return "hi";
+                }
+
+            };
+        }
+    ]);
+
 }());
