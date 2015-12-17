cfaa5446296283656c8c5af40a3cdc2de883edb0
waafre13 <waafre13@student.westerdals.no>
2015-12-17 15:59:45 +0100

    Added index.html, scripts, and app.js

---
+++ MyMoviesApp/Scripts/App/app.js
@@ -0,0 +1,19 @@
+﻿
+(function() {
+
+    var myMoviesApp = angular.module("MyMoviesApp", []);
+
+    myMoviesApp.controller("MainController", [
+        "$http", "$scope", function($http, $scope) {
+            alert("ALERT!");
+            $scope.test = "Fredric";
+
+            //API Urls
+
+            //
+
+        }
+    ]);
+
+
+}());
